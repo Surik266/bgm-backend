463ab410779e3ee7593ab4c7047835ce9df31cde
Surik266 <surik.pozoyan@gmail.com>
2020-03-21 11:51:06 +0400

    updated port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 app.get('/', function (req, res) {
     res.send('<h1>Hello World from Nodejs!</h1>');
 });
-const server = app.listen(8888, function () {
+const server = app.listen(80, function () {
     const host = server.address().address;
     const port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
