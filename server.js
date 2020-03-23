00564765b55a5b2738d1783a35da8f5a6070ffd7
Surik266 <surik.pozoyan@gmail.com>
2020-03-23 06:43:49 +0400

    removed arrow functions

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const PORT = 8888;
 
 const app = express();
 
-app.use('/dashboard', (req, res, next) => {
-        res.redirect('/')
+app.use('/dashboard',function (req, res, next) {
+    res.redirect('/')
 });
 
 app.use(express.static(path.resolve(__dirname, './build')));
 
-app.listen(PORT, () => {
+app.listen(PORT, function () {
     console.log('app launched on ' + PORT)
 });
